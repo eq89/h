94dc4b69e1a33928751612df3cdca951a6313e57
eq89 <156458521+eq89@users.noreply.github.com>
2024-01-26 20:49:34 +0500

    Update au.js

--- au.js
+++ au.js
@@ -1,21 +1,37 @@
-const socket = new WebSocket('wss://fly-py.fly.dev/');
+const socketURL = 'wss://fly-py.fly.dev/';
 const audio = new Audio();
+let socket;
 
-socket.addEventListener('open', (event) => {
-  socket.send('Connected!');
-});
+function connectWebSocket() {
+  socket = new WebSocket(socketURL);
 
-socket.addEventListener('message', (event) => {
-  handleAudio(event.data);
-});
+  socket.addEventListener("open", (event) => {
+    console.log("WebSocket connection opened:", event);
+    socket.send("Connected!");
+  });
 
-function handleAudio(audioUrl) {
-  if (audioUrl.includes('#')) {
+  socket.addEventListener("message", (event) => {
+    playAudio(event.data);
+  });
+
+  socket.addEventListener("close", (event) => {
+    setTimeout(connectWebSocket, 2000);
+  });
+
+  socket.addEventListener("error", (event) => {
+    setTimeout(connectWebSocket, 2000);
+  });
+}
+
+function playAudio(audioUrl) {
+  if (audioUrl.includes("#")) {
     audio.pause();
-		audio.currentTime = 0;
-  	return;
+    audio.currentTime = 0;
+    return;
   }
 
-  audio.src = audioUrl + '?raw=true';
+  audio.src = audioUrl + "?raw=true";
   audio.play();
 }
+
+connectWebSocket();
