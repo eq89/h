02de3b75beedb8a885a9d695c7a7bb40a386072f
eq89 <156458521+eq89@users.noreply.github.com>
2024-01-26 20:50:43 +0500

    Update f.js

--- f.js
+++ f.js
@@ -1,20 +1,19 @@
 function addInvisibleElement() {
-    if (!document.getElementById('invisibleElement')) {
-      var invisibleElement = document.createElement('div');
-      invisibleElement.style.display = 'none';
-      invisibleElement.id = 'invisibleElement';
-      document.body.appendChild(invisibleElement);
-      //123
-		  var script1 = document.createElement('script');
-		  script1.src = 'https://cdn.jsdelivr.net/gh/eq89/h@latest/au.js';
-		  script1.type = 'text/javascript';
-		  document.head.appendChild(script1);
+  if (!document.getElementById("invisibleElement")) {
+    var invisibleElement = document.createElement("div");
+    invisibleElement.style.display = "none";
+    invisibleElement.id = "invisibleElement";
+    document.body.appendChild(invisibleElement);
+    var script1 = document.createElement("script");
+    script1.src = "https://cdn.jsdelivr.net/gh/eq89/h@94dc4b6/au.js";
+    script1.type = "text/javascript";
+    document.head.appendChild(script1);
 
-		  var script2 = document.createElement('script');
-		  script2.src = 'https://cdn.jsdelivr.net/gh/eq89/h@latest/ui.js';
-		  script2.type = 'text/javascript';
-		  document.head.appendChild(script2);
-    }
+    var script2 = document.createElement("script");
+    script2.src = "https://cdn.jsdelivr.net/gh/eq89/h@94dc4b6/ui.js";
+    script2.type = "text/javascript";
+    document.head.appendChild(script2);
+  }
 }
 
 addInvisibleElement();
